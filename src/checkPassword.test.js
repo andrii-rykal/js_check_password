c8b4ad9a59e9cb98519912a40ca15b1625031688
Andrii Rykal <andreuka79@gmail.com>
2024-09-12 14:04:34 +0300

    Solution

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,52 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
+    const boolean = checkPassword('');
 
+    expect(typeof boolean).toBe('boolean');
   });
 
   it(`should return 'true' for the valid password with 8 characters`, () => {
+    const truthy = checkPassword('A1@pasword');
 
+    expect(truthy).toBeTruthy();
   });
 
-  // write more tests here
+  it(`should return 'false' for a password that contains`
+    + ` non-English characters`, () => {
+    const falsy = checkPassword('Я1@pasword');
+
+    expect(falsy).toBeFalsy();
+  });
+
+  it(`should return "false" for a password of less than 8 characters`, () => {
+    const falsy = checkPassword('A1@pas');
+
+    expect(falsy).toBeFalsy();
+  });
+
+  it(`should return "false" for a password longer than 16 characters`, () => {
+    const falsy = checkPassword('Pasword1@pasword18');
+
+    expect(falsy).toBeFalsy();
+  });
+
+  it(`should return "false" for a password without number`, () => {
+    const falsy = checkPassword('Pasword@pasword');
+
+    expect(falsy).toBeFalsy();
+  });
+
+  it(`should return "false" for a password without special character`, () => {
+    const falsy = checkPassword('Pasword1');
+
+    expect(falsy).toBeFalsy();
+  });
+
+  it(`should return "false" for a password`
+    + ` that does not contain an uppercase letter`, () => {
+    const falsy = checkPassword('pasword@1');
+
+    expect(falsy).toBeFalsy();
+  });
 });
